4cf4afb316bae4cb1b810f12d00b237212850047
Daniel Paiva <danielbrug@gmail.com>
2024-03-13 22:41:29 -0300

    Add build from source libpqxx

--- _cmake/PQXX.cmake
+++ _cmake/PQXX.cmake
@@ -2,12 +2,16 @@ include(FetchContent)
 set(SKIP_BUILD_TEST ON)
 set(INSTALL_TEST OFF)
 #---------------------------------------------------------------------------------------------
-FetchContent_Declare(
-        pqxx
-        GIT_REPOSITORY https://github.com/jtv/libpqxx.git
-        GIT_TAG 7.8.1
-)
-FetchContent_MakeAvailable(pqxx)
-FetchContent_GetProperties(pqxx)
+if(BUILD_FROM_SRC)
+    FetchContent_Declare(
+            pqxx
+            GIT_REPOSITORY https://github.com/jtv/libpqxx.git
+            GIT_TAG 7.8.1
+    )
+    FetchContent_MakeAvailable(pqxx)
+    FetchContent_GetProperties(pqxx)
+else ()
+    find_package(libpqxx REQUIRED)
+endif ()
 #---------------------------------------------------------------------------------------------
-target_link_libraries(${PROJECT_NAME} PUBLIC pqxx)
+target_link_libraries(${PROJECT_NAME} PUBLIC libpqxx::pqxx)
