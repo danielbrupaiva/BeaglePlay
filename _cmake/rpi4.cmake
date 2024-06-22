7e8b32dc77d1c61494cc71dc3b5a8af0c6695e2b
Daniel Paiva <danielbrug@gmail.com>
2024-06-22 12:02:42 -0300

    WIP rpi4 toolchain

--- _cmake/rpi4.cmake
+++ _cmake/rpi4.cmake
@@ -2,22 +2,32 @@
 set(CROSS_COMPILER ON)
 option(CROSS_COMPILER "Enable cross compiler?" ON)
 message("${BoldYellow}-- TOOLCHAIN LOADED${ColourReset}")
-set(TARGET_ARCHITECTURE aarch64-linux-gnu)
+
+include_guard(GLOBAL)
+
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
 # define target sysroot
-set(CMAKE_SYSROOT /home/daniel/CLionProjects/sysroots/rpi4)
+set(TARGET_SYSROOT /home/daniel/CLionProjects/sysroots/rpi4)
+set(CMAKE_SYSROOT ${TARGET_SYSROOT})
+
+set(ENV{PKG_CONFIG_PATH} $PKG_CONFIG_PATH:/usr/lib/aarch64-linux-gnu/pkgconfig)
+set(ENV{PKG_CONFIG_LIBDIR} /usr/lib/pkgconfig:/usr/share/pkgconfig/:${TARGET_SYSROOT}/usr/lib/aarch64-linux-gnu/pkgconfig:${TARGET_SYSROOT}/usr/lib/pkgconfig)
+set(ENV{PKG_CONFIG_SYSROOT_DIR} ${CMAKE_SYSROOT})
+
 # define compilers
 set(CMAKE_C_COMPILER /usr/bin/aarch64-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER /usr/bin/aarch64-linux-gnu-g++)
 # define C/CXX flags
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -I${TARGET_SYSROOT}/usr/include")
 set(CMAKE_CXX_FLAGS ${CMAKE_C_FLAGS})
+# Qt specifics
+set(QT_COMPILER_FLAGS "-march=armv8-a")
+set(QT_COMPILER_FLAGS_RELEASE "-O2 -pipe")
+set(QT_LINKER_FLAGS "-Wl,-O1 -Wl,--hash-style=gnu -Wl,--as-needed")
 # define compiler work (used by custom toolchain)
 set(CMAKE_C_COMPILER_WORKS ON)
 set(CMAKE_CXX_COMPILER_WORKS ON)
-# compiler/linker flags
-#add_definitions("--sysroot=${CMAKE_SYSROOT}")
-#set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} --sysroot=${CMAKE_SYSROOT}" CACHE INTERNAL "" FORCE)
-#set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --sysroot=${CMAKE_SYSROOT}" CACHE INTERNAL "" FORCE)
 
 set(CMAKE_FIND_ROOT_PATH ${CMAKE_SYSROOT})
 # search for programs in the build host directories
@@ -26,3 +36,28 @@ SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+set(CMAKE_BUILD_RPATH ${TARGET_SYSROOT})
+
+include(CMakeInitializeConfigs)
+
+function(cmake_initialize_per_config_variable _PREFIX _DOCSTRING)
+    if (_PREFIX MATCHES "CMAKE_(C|CXX|ASM)_FLAGS")
+        set(CMAKE_${CMAKE_MATCH_1}_FLAGS_INIT "${QT_COMPILER_FLAGS}")
+
+        foreach (config DEBUG RELEASE MINSIZEREL RELWITHDEBINFO)
+            if (DEFINED QT_COMPILER_FLAGS_${config})
+                set(CMAKE_${CMAKE_MATCH_1}_FLAGS_${config}_INIT "${QT_COMPILER_FLAGS_${config}}")
+            endif()
+        endforeach()
+    endif()
+
+
+    if (_PREFIX MATCHES "CMAKE_(SHARED|MODULE|EXE)_LINKER_FLAGS")
+        foreach (config SHARED MODULE EXE)
+            set(CMAKE_${config}_LINKER_FLAGS_INIT "${QT_LINKER_FLAGS}")
+        endforeach()
+    endif()
+
+    _cmake_initialize_per_config_variable(${ARGV})
+endfunction()
