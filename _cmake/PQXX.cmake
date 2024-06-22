1925f7a8aff47350a5c1e9c5edbff0622a12d914
Daniel Paiva <danielbrug@gmail.com>
2024-06-22 11:25:08 -0300

    WIP rpi4 toolchain

--- _cmake/PQXX.cmake
+++ _cmake/PQXX.cmake
@@ -1,22 +1,21 @@
-if(CROSS_COMPILER)
-    message("${BoldYellow}-- CROSS_COMPILER: ${CROSS_COMPILER}${ColourReset}")
-    set(PostgreSQL_FOUND ON)
-    set(PostgreSQL_LIBRARY
-            ${CMAKE_SYSROOT}/usr/lib/aarch64-linux-gnu/libpq.so
-            ${CMAKE_SYSROOT}/usr/lib/aarch64-linux-gnu/libpq.a
-    )
-    set(PostgreSQL_INCLUDE_DIRS ${CMAKE_SYSROOT}/usr/include/postgresql)
-    set(PostgreSQL_LIBRARY_DIRS ${CMAKE_SYSROOT}/usr/lib/postgresql)
-    target_include_directories(${PROJECT_NAME} PUBLIC ${PostgreSQL_INCLUDE_DIRS})
-    target_link_directories(${PROJECT_NAME} PUBLIC ${PostgreSQL_LIBRARY_DIRS})
-    target_link_libraries(${PROJECT_NAME} PUBLIC ${PostgreSQL_LIBRARY})
-endif ()
-
-set(SKIP_BUILD_TEST ON)
-set(INSTALL_TEST OFF)
+#if(CROSS_COMPILER)
+#    message("${BoldYellow}-- CROSS_COMPILER: ${CROSS_COMPILER}${ColourReset}")
+#    set(PostgreSQL_FOUND ON)
+#    set(PostgreSQL_LIBRARY
+#            ${CMAKE_SYSROOT}/usr/lib/aarch64-linux-gnu/libpq.so
+#            ${CMAKE_SYSROOT}/usr/lib/aarch64-linux-gnu/libpq.a
+#    )
+#    set(PostgreSQL_INCLUDE_DIRS ${CMAKE_SYSROOT}/usr/include/postgresql)
+#    set(PostgreSQL_LIBRARY_DIRS ${CMAKE_SYSROOT}/usr/lib/postgresql)
+#    target_include_directories(${PROJECT_NAME} PUBLIC ${PostgreSQL_INCLUDE_DIRS})
+#    target_link_directories(${PROJECT_NAME} PUBLIC ${PostgreSQL_LIBRARY_DIRS})
+#    target_link_libraries(${PROJECT_NAME} PUBLIC ${PostgreSQL_LIBRARY})
+#endif ()
 #---------------------
 message("${BoldYellow}-- PQXX${ColourReset}")
 if(BUILD_FROM_SRC)
+    set(SKIP_BUILD_TEST ON)
+    set(INSTALL_TEST OFF)
     message("${BoldYellow}-- BUILDING FROM SRC${ColourReset}")
     include(FetchContent)
     FetchContent_Declare(
