79515610fb0d8ca8d8ef7ac213ab65925e8a1373
Daniel Paiva <danielbrug@gmail.com>
2024-06-22 08:51:11 -0300

    WIP rpi4 toolchain

---
+++ _cmake/rpi4.cmake
@@ -0,0 +1,28 @@
+#-DCMAKE_TOOLCHAIN_FILE=/home/daniel/CLionProjects/BeaglePlay/_cmake/rpi4.cmake
+set(CROSS_COMPILER ON)
+option(CROSS_COMPILER "Enable cross compiler?" ON)
+message("${BoldYellow}-- TOOLCHAIN LOADED${ColourReset}")
+set(TARGET_ARCHITECTURE aarch64-linux-gnu)
+# define target sysroot
+set(CMAKE_SYSROOT /home/daniel/CLionProjects/sysroots/rpi4/)
+# define compilers
+set(CMAKE_C_COMPILER /usr/bin/aarch64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER /usr/bin/aarch64-linux-gnu-g++)
+# define C/CXX flags
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
+set(CMAKE_CXX_FLAGS ${CMAKE_C_FLAGS})
+# define compiler work (used by custom toolchain)
+set(CMAKE_C_COMPILER_WORKS ON)
+set(CMAKE_CXX_COMPILER_WORKS ON)
+# compiler/linker flags
+add_definitions("--sysroot=${CMAKE_SYSROOT}")
+set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} --sysroot=${CMAKE_SYSROOT}" CACHE INTERNAL "" FORCE)
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --sysroot=${CMAKE_SYSROOT}" CACHE INTERNAL "" FORCE)
+
+set(CMAKE_FIND_ROOT_PATH ${CMAKE_SYSROOT})
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+#set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
