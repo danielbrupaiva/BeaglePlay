89b1fa2d948d0fe4c63c7279e041f2dec2c2077e
Daniel Paiva <danielbrug@gmail.com>
2024-06-26 20:51:47 -0300

    Reviewing FSM

--- App/globals.hpp
+++ App/globals.hpp
@@ -1,15 +1,65 @@
 #pragma once
-#include "FSM.hpp"
 #include "logger.hpp"
 #include "user.hpp"
 #include "database.hpp"
 #include "image.hpp"
+
 static Core::Logger logger{"App",spdlog::level::debug};
 
-/*GLOBAL VARIABLES*/
 namespace Global {
-    static std::shared_ptr<Core::Database> db = Core::Database::create("localhost", "5432", "imgui", "1234", "project");
-    static Core::User system_user;
-    static std::map< std::string, GLuint > GL_textures_resources;
+/*GLOBAL VARIABLES*/
+static std::shared_ptr<Core::Database> db = Core::Database::create("localhost", "5432", "imgui", "1234", "project");
+static Core::User system_user;
+static std::map< std::string, GLuint > GL_textures_resources;
+//FSM definition
+#define NUM_OF_STATES 5
+enum class eSystemState { SCREEN1, SCREEN2, SCREEN3, SCREEN4, SCREEN5 };
+
+static eSystemState current_state = eSystemState::SCREEN1;
+static bool isTransition[NUM_OF_STATES];
+
+struct SystemState_t {
+    eSystemState currentState;
+    bool (*transition);
+    eSystemState nextState[2];
+    void (*pfHandler)();
+    unsigned int delayTime = 0U;
+};
+
+void screen1_render();
+void screen2_render();
+void screen3_render();
+void screen4_render();
+void screen5_render();
+
+std::vector<SystemState_t> FSM = {
+    { eSystemState::SCREEN1, &isTransition[static_cast<int>(eSystemState::SCREEN1)], {eSystemState::SCREEN2, eSystemState::SCREEN5}, &screen1_render, 0U },
+    { eSystemState::SCREEN2, &isTransition[static_cast<int>(eSystemState::SCREEN2)], {eSystemState::SCREEN3, eSystemState::SCREEN1}, &screen2_render, 0U },
+    { eSystemState::SCREEN3, &isTransition[static_cast<int>(eSystemState::SCREEN3)], {eSystemState::SCREEN4, eSystemState::SCREEN2}, &screen3_render, 0U },
+    { eSystemState::SCREEN4, &isTransition[static_cast<int>(eSystemState::SCREEN4)], {eSystemState::SCREEN5, eSystemState::SCREEN3}, &screen4_render, 0U },
+    { eSystemState::SCREEN5, &isTransition[static_cast<int>(eSystemState::SCREEN5)], {eSystemState::SCREEN1, eSystemState::SCREEN4}, &screen5_render, 0U }
+};
+
 
 };//namespace Global
+
+void Global::screen1_render()
+{
+
+}
+void Global::screen2_render()
+{
+
+}
+void Global::screen3_render()
+{
+
+}
+void Global::screen4_render()
+{
+
+}
+void Global::screen5_render()
+{
+
+};
