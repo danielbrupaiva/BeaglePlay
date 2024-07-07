8499171d24b8b7b5aaf9112df00a8ecbb82a23cc
Daniel Paiva <danielbrug@gmail.com>
2024-07-07 16:34:21 -0300

    WIP Screen1 layout

--- App/screen_1.hpp
+++ App/screen_1.hpp
@@ -19,9 +19,16 @@ public:
         float x_offset = (control_size.x - size.x) * 0.5f;
         float y_offset = (control_size.y - size.y) * 0.5f;
 
+        ImGui::PushID(1);
+        ImGui::PushStyleColor(ImGuiCol_Button,          ImVec4(0.173f, 0.173f, 0.173f, 0.0f));
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered,   ImVec4(0.271f, 0.271f, 0.271f, 0.0f));
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive,    ImVec4(0.271f, 0.271f, 0.271f, 0.0f));
+
         ImGui::SetCursorPos(ImVec2(position.x + x_offset, position.y + y_offset));
         if(ImGui::ImageButton("LOGO", Global::GL_Textures["logo"]->ID(), Global::GL_Textures["logo"]->resize())) {
             Global::current_state = Global::eSystemState::SCREEN2;
         }
+        ImGui::PopStyleColor(3);
+        ImGui::PopID();
     }
 };
