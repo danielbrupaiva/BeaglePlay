c0546ef12d8b8615a831108b1bbdfac08be61ebd
Daniel Paiva <danielbrug@gmail.com>
2024-07-07 16:10:00 -0300

    Add project imgui style

--- App/ui.hpp
+++ App/ui.hpp
@@ -80,7 +80,7 @@ private:
 
         colors[ImGuiCol_Text]                   = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
         colors[ImGuiCol_TextDisabled]           = ImVec4(0.50f, 0.50f, 0.50f, 1.00f);
-        colors[ImGuiCol_WindowBg]               = ImVec4(0.109f, 0.109f, 0.109f, 0.0f);
+        colors[ImGuiCol_WindowBg]               = ImVec4(0.109f, 0.109f, 0.109f, 1.0f);
         colors[ImGuiCol_ChildBg]                = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
         colors[ImGuiCol_PopupBg]                = ImVec4(0.08f, 0.08f, 0.08f, 0.94f);
         colors[ImGuiCol_Border]                 = ImVec4(0.43f, 0.43f, 0.50f, 0.50f);
