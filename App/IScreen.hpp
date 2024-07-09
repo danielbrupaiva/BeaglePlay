8e43929fda5758e8ca6a301b94674cdd5369c921
Daniel Paiva <danielbrug@gmail.com>
2024-07-09 17:09:43 -0300

    Add set_position method

--- App/IScreen.hpp
+++ App/IScreen.hpp
@@ -5,12 +5,20 @@
 
 class IScreen{
 protected:
-    ImVec2 m_size;
+    ImVec2 m_window_size;
     ImVec4 m_bg_color;
     std::string m_name;
+    ImVec2 m_control_size;
 public:
-    explicit IScreen(std::string_view name, const ImVec2 &size, const ImVec4 &bg_color)
-        : m_name{name}, m_size{size}, m_bg_color{bg_color}
+    explicit IScreen(std::string_view name, const ImVec2 &window_size, const ImVec4 &bg_color)
+        : m_name{name}, m_window_size{window_size}, m_bg_color{bg_color}
     {}
     virtual void render(App::UI& app) = 0;
+
+    static void set_position(ImVec2 window_size, ImVec2 control_size, ImVec2 size, ImVec2 offset = {0.5f, 0.5f}, ImVec2 factor = {0.5f, 0.5f})
+    {
+        ImVec2 position{(window_size.x - control_size.x) * factor.x + (window_size.x - size.x) * offset.x,
+                        (window_size.y - control_size.y) * factor.y + (window_size.y - size.y) * offset.y};
+        ImGui::SetCursorPos(position);
+    }
 };
