8d11f5122324c29e3a5309940aacbb854fb70c5f
Daniel Paiva <danielbrug@gmail.com>
2024-07-11 22:40:46 -0300

    Add Login Popup

--- App/screen_1.hpp
+++ App/screen_1.hpp
@@ -19,7 +19,69 @@ public:
             ImGui::PushStyleColor(ImGuiCol_ButtonHovered,   ImVec4(0.271f, 0.271f, 0.271f, 0.0f));
             ImGui::PushStyleColor(ImGuiCol_ButtonActive,    ImVec4(0.271f, 0.271f, 0.271f, 0.0f));
 
-            if(ImGui::Button("##", m_control_size)) { Global::current_state = Global::eSystemState::SCREEN2; }
+            static bool login_window = false;
+
+            if(ImGui::Button("##", m_control_size)) {
+
+                if (Global::system_user.get_is_logged()) {
+                    Global::current_state = Global::eSystemState::SCREEN2;
+                }
+                else {
+                    ImGui::OpenPopup("LoginWindow");
+                }
+            }
+
+            if (ImGui::BeginPopupModal("LoginWindow", NULL, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove))
+            {
+                {
+                    ImVec2 size{48.0f, 48.0f};
+                    ImVec2 position = center_position(ImGui::GetWindowSize(), ImGui::GetContentRegionAvail(), size, ImVec2(0.5f, 0.0f));
+                    ImGui::SetCursorPosX(position.x);
+                    ImGui::Image(Global::GL_Textures["profile"]->ID(), size);
+                }
+                {
+                    std::string text = "WELCOME";
+                    ImVec2 size = ImGui::CalcTextSize(text.c_str());
+                    ImVec2 position = center_position(ImGui::GetWindowSize(), ImGui::GetContentRegionAvail(), size, ImVec2(0.5f, 0.0f));
+                    ImGui::SetCursorPosX(position.x);
+                    ImGui::Text("%s", text.c_str());
+                }
+                {
+                    std::string text = Global::system_user.get_name();
+                    ImVec2 size = ImGui::CalcTextSize(text.c_str());
+                    ImVec2 position = center_position(ImGui::GetWindowSize(), ImGui::GetContentRegionAvail(), size, ImVec2(0.5f, 0.0f));
+                    ImGui::SetCursorPosX(position.x);
+                    ImGui::Text("%s", text.c_str());
+                }
+                {
+                    /** Username InputText field **/
+                    ImGui::Text("Username");
+                    static std::string username;
+                    ImGui::InputTextWithHint("##username", "Enter username", username.data(), sizeof(username.data()),
+                                             ImGuiInputTextFlags_CharsUppercase | ImGuiInputTextFlags_CharsNoBlank);
+                }
+                {/** Password InputText field **/
+                    ImGui::Text("Password");
+                    static std::string password;
+                    static bool isViewPass = false;
+                    ImGui::InputTextWithHint("##password:", "Enter password", password.data(), sizeof(password.data()),
+                                             isViewPass ? ImGuiInputTextFlags_None : ImGuiInputTextFlags_CharsUppercase | ImGuiInputTextFlags_Password);
+                    ImGui::SameLine();
+                    ImGui::Checkbox("View", &isViewPass);
+                }
+                {/** Login | Logout button **/
+                    if(ImGui::Button(Global::system_user.get_is_logged() ? "Logout" : "Login")){ }
+                }
+                {
+                    std::string text = "Close";
+                    ImVec2 size = ImGui::CalcTextSize(text.c_str());
+                    ImVec2 position = center_position(ImGui::GetWindowSize(), ImGui::GetContentRegionAvail(), size, ImVec2(0.5f, 0.0f));
+                    ImGui::SetCursorPosX(position.x);
+                    if (ImGui::Button(text.c_str())) { ImGui::CloseCurrentPopup(); }
+                }
+
+                ImGui::EndPopup();
+            }
 
             ImGui::PopStyleColor(3);
         }ImGui::PopID();
