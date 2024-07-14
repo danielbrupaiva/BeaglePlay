17ac987e058678bde42ad5bed0c8f99933ed207d
Daniel Paiva <danielbrug@gmail.com>
2024-07-13 21:19:13 -0300

    Set frame colors according project

--- App/ui.hpp
+++ App/ui.hpp
@@ -108,11 +108,11 @@ private:
         colors[ImGuiCol_PopupBg]                = ImVec4(0.08f, 0.08f, 0.08f, 0.94f);
         colors[ImGuiCol_Border]                 = ImVec4(0.43f, 0.43f, 0.50f, 0.50f);
         colors[ImGuiCol_BorderShadow]           = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
-        colors[ImGuiCol_FrameBg]                = ImVec4(0.16f, 0.29f, 0.48f, 0.54f);
-        colors[ImGuiCol_FrameBgHovered]         = ImVec4(0.26f, 0.59f, 0.98f, 0.40f);
-        colors[ImGuiCol_FrameBgActive]          = ImVec4(0.26f, 0.59f, 0.98f, 0.67f);
-        colors[ImGuiCol_TitleBg]                = ImVec4(0.04f, 0.04f, 0.04f, 1.00f);
-        colors[ImGuiCol_TitleBgActive]          = ImVec4(0.16f, 0.29f, 0.48f, 1.00f);
+        colors[ImGuiCol_FrameBg]                = ImVec4(0.173f, 0.173f, 0.173f, 1.0f);
+        colors[ImGuiCol_FrameBgHovered]         = ImVec4(0.271f, 0.271f, 0.271f, 1.0f);
+        colors[ImGuiCol_FrameBgActive]          = ImVec4(0.271f, 0.271f, 0.271f, 1.0f);
+        colors[ImGuiCol_TitleBg]                = ImVec4(0.173f, 0.173f, 0.173f, 1.0f);
+        colors[ImGuiCol_TitleBgActive]          = ImVec4(0.173f, 0.173f, 0.173f, 1.0f);
         colors[ImGuiCol_TitleBgCollapsed]       = ImVec4(0.00f, 0.00f, 0.00f, 0.51f);
         colors[ImGuiCol_MenuBarBg]              = ImVec4(0.14f, 0.14f, 0.14f, 1.00f);
         colors[ImGuiCol_ScrollbarBg]            = ImVec4(0.02f, 0.02f, 0.02f, 0.53f);
