d8ac9a4871e15d9b6b61a95586558c55b5ba8a1e
Daniel Paiva <danielbrug@gmail.com>
2024-07-15 15:38:12 -0300

    Add operation buttons

--- App/screen_2.hpp
+++ App/screen_2.hpp
@@ -12,6 +12,109 @@ public:
 
     void render(App::UI& app) override
     {
+        {/**Operation buttons **/
+            static const uint8_t number_of_buttons = 6;
 
+            static ImVec2 size{ (ImGui::GetWindowWidth() - 2 * ImGui::GetStyle().FramePadding.x - (number_of_buttons - 1 ) * ImGui::GetStyle().ItemSpacing.x ) / (float) number_of_buttons,
+                                ImGui::GetWindowHeight() * 0.12f};
+
+            ImGui::SetCursorPosY(ImGui::GetWindowHeight() - size.y - ImGui::GetStyle().FramePadding.y);
+            /**Button 1**/
+            static bool is_btn1_active = false;
+            if(ImGui::Button("BUTTON #1", size)) {
+                is_btn1_active = !is_btn1_active;
+                button_1_callback();
+            }
+            if(is_btn1_active)
+            {// Calculate position for the line and text
+                static ImVec2 pos = ImGui::GetItemRectMin();
+                static float lineThickness = 8.0f;
+                ImVec2 lineStart = ImVec2(pos.x + ImGui::GetStyle().FramePadding.x, pos.y + ImGui::GetStyle().FramePadding.y);
+                ImVec2 lineEnd = ImVec2(pos.x + size.x - ImGui::GetStyle().FramePadding.x, pos.y + ImGui::GetStyle().FramePadding.y);
+                ImGui::GetWindowDrawList()->AddLine(lineStart, lineEnd, IM_COL32(0, 204, 0, 255), lineThickness);
+            }
+            ImGui::SameLine();
+            /**Button 2**/
+            static bool is_btn2_active = false;
+            if(ImGui::Button("BUTTON #2", size)) {
+                is_btn2_active = !is_btn2_active;
+                button_2_callback();
+            }
+            if(is_btn2_active)
+            {// Calculate position for the line and text
+                static ImVec2 pos = ImGui::GetItemRectMin();
+                static float lineThickness = 8.0f;
+                ImVec2 lineStart = ImVec2(pos.x + ImGui::GetStyle().FramePadding.x, pos.y + ImGui::GetStyle().FramePadding.y);
+                ImVec2 lineEnd = ImVec2(pos.x + size.x - ImGui::GetStyle().FramePadding.x, pos.y + ImGui::GetStyle().FramePadding.y);
+                ImGui::GetWindowDrawList()->AddLine(lineStart, lineEnd, IM_COL32(0, 204, 0, 255), lineThickness);
+            }
+            ImGui::SameLine();
+            /**Button 3**/
+            static bool is_btn3_active = false;
+            if(ImGui::Button("BUTTON #3", size)) {
+                is_btn3_active = !is_btn3_active;
+                button_3_callback();
+            }
+            if(is_btn3_active)
+            {// Calculate position for the line and text
+                static ImVec2 pos = ImGui::GetItemRectMin();
+                static float lineThickness = 8.0f;
+                ImVec2 lineStart = ImVec2(pos.x + ImGui::GetStyle().FramePadding.x, pos.y + ImGui::GetStyle().FramePadding.y);
+                ImVec2 lineEnd = ImVec2(pos.x + size.x - ImGui::GetStyle().FramePadding.x, pos.y + ImGui::GetStyle().FramePadding.y);
+                ImGui::GetWindowDrawList()->AddLine(lineStart, lineEnd, IM_COL32(0, 204, 0, 255), lineThickness);
+            }
+            ImGui::SameLine();
+            /**Button 4**/
+            static bool is_btn4_active = false;
+            if(ImGui::Button("BUTTON #4", size)) {
+                is_btn4_active = !is_btn4_active;
+                button_4_callback();
+            }
+            if(is_btn4_active)
+            {// Calculate position for the line and text
+                static ImVec2 pos = ImGui::GetItemRectMin();
+                static float lineThickness = 8.0f;
+                ImVec2 lineStart = ImVec2(pos.x + ImGui::GetStyle().FramePadding.x, pos.y + ImGui::GetStyle().FramePadding.y);
+                ImVec2 lineEnd = ImVec2(pos.x + size.x - ImGui::GetStyle().FramePadding.x, pos.y + ImGui::GetStyle().FramePadding.y);
+                ImGui::GetWindowDrawList()->AddLine(lineStart, lineEnd, IM_COL32(0, 204, 0, 255), lineThickness);
+            }
+            ImGui::SameLine();
+            /**Button 5**/
+            static bool is_btn5_active = false;
+            if(ImGui::Button("BUTTON #5", size)) {
+                is_btn5_active = !is_btn5_active;
+                button_5_callback();
+            }
+            if(is_btn5_active)
+            {// Calculate position for the line and text
+                static ImVec2 pos = ImGui::GetItemRectMin();
+                static float lineThickness = 8.0f;
+                ImVec2 lineStart = ImVec2(pos.x + ImGui::GetStyle().FramePadding.x, pos.y + ImGui::GetStyle().FramePadding.y);
+                ImVec2 lineEnd = ImVec2(pos.x + size.x - ImGui::GetStyle().FramePadding.x, pos.y + ImGui::GetStyle().FramePadding.y);
+                ImGui::GetWindowDrawList()->AddLine(lineStart, lineEnd, IM_COL32(0, 204, 0, 255), lineThickness);
+            }
+            ImGui::SameLine();
+            /**Button 6**/
+            static bool is_btn6_active = false;
+            if(ImGui::Button("BUTTON #6", size)) {
+                is_btn6_active = !is_btn6_active;
+                button_6_callback();
+            }
+            if(is_btn6_active)
+            {// Calculate position for the line and text
+                static ImVec2 pos = ImGui::GetItemRectMin();
+                static float lineThickness = 8.0f;
+                ImVec2 lineStart = ImVec2(pos.x + ImGui::GetStyle().FramePadding.x, pos.y + ImGui::GetStyle().FramePadding.y);
+                ImVec2 lineEnd = ImVec2(pos.x + size.x - ImGui::GetStyle().FramePadding.x, pos.y + ImGui::GetStyle().FramePadding.y);
+                ImGui::GetWindowDrawList()->AddLine(lineStart, lineEnd, IM_COL32(0, 204, 0, 255), lineThickness);
+            }
+        }
     }
+private:
+    static void button_1_callback(){ logger.debug("Button #1 callback");}
+    static void button_2_callback(){ logger.debug("Button #2 callback");}
+    static void button_3_callback(){ logger.debug("Button #3 callback");}
+    static void button_4_callback(){ logger.debug("Button #4 callback");}
+    static void button_5_callback(){ logger.debug("Button #5 callback");}
+    static void button_6_callback(){ logger.debug("Button #6 callback");}
 };
