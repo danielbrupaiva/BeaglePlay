a3101e3a8f11b00629bdbf27152ffd91e5c25761
Daniel Paiva <danielbrug@gmail.com>
2024-07-15 20:58:19 -0300

    WIP screen2

--- App/screen_2.hpp
+++ App/screen_2.hpp
@@ -8,43 +8,44 @@
 #include "IScreen.hpp"
 
 class Screen2 : public IScreen{
-
-    std::vector<Button> buttons = {
-        {1, "BUTTON#1", false, &button_1_callback},
-        {2, "BUTTON#2", false, &button_2_callback},
-        {3, "BUTTON#3", false, &button_3_callback},
-        {4, "BUTTON#4", false, &button_4_callback},
-        {5, "BUTTON#5", false, &button_5_callback},
-        {6, "BUTTON#6", false, &button_6_callback}
-    };
-
+    std::vector<Button> buttons ;
 public:
     Screen2(std::string_view name, const ImVec2 &window_size, const ImVec4 &bg_color)
         : IScreen{name, window_size, bg_color}
-    {}
+    {
+        buttons = {
+            {1, "BUTTON#1", false, &button_1_callback, {204, 86.4000015}},
+            {2, "BUTTON#2", false, &button_2_callback, {204, 86.4000015}},
+            {3, "BUTTON#3", false, &button_3_callback, {204, 86.4000015}},
+            {4, "BUTTON#4", false, &button_4_callback, {204, 86.4000015}},
+            {5, "BUTTON#5", false, &button_5_callback, {204, 86.4000015}},
+            {6, "BUTTON#6", false, &button_6_callback, {204, 86.4000015}}
+        };
+    }
 
     void render(App::UI& app) override
     {
         {/**Operation buttons **/
-            static const uint32_t NUM_BUTTONS = 6;
-            static ImVec2 size{ (ImGui::GetWindowWidth() - 2 * ImGui::GetStyle().FramePadding.x - (NUM_BUTTONS - 1 ) * ImGui::GetStyle().ItemSpacing.x ) / (float) NUM_BUTTONS,
+            static ImVec2 size{ (ImGui::GetWindowWidth() - 2 * ImGui::GetStyle().FramePadding.x - (float)(buttons.size() - 1) * ImGui::GetStyle().ItemSpacing.x ) / (float)buttons.size(),
                                 ImGui::GetWindowHeight() * 0.12f};
             ImGui::SetCursorPosY(ImGui::GetWindowHeight() - size.y - ImGui::GetStyle().FramePadding.y);
 
-            std::for_each(buttons.begin(), buttons.end(), [](auto& button){
+            std::for_each(buttons.begin(), buttons.end(), [&](auto& button){
                 if(ImGui::Button(button.label.c_str(), size)) {
                     TOGGLE(button.state);
                     button.callback();
                 }
                 if(button.state)
-                {// Calculate position for the line and text
+                {   // Calculate position for the line and text
                     ImVec2 pos = ImGui::GetItemRectMin();
                     static float lineThickness = 8.0f;
                     ImVec2 lineStart = ImVec2(pos.x + ImGui::GetStyle().FramePadding.x, pos.y + ImGui::GetStyle().FramePadding.y);
                     ImVec2 lineEnd = ImVec2(pos.x + size.x - ImGui::GetStyle().FramePadding.x, pos.y + ImGui::GetStyle().FramePadding.y);
                     ImGui::GetWindowDrawList()->AddLine(lineStart, lineEnd, IM_COL32(0, 204, 0, 255), lineThickness);
                 }
-                ImGui::SameLine();
+                if(button.id != buttons.end()->id){
+                    ImGui::SameLine();
+                }
             });
         }
     }
