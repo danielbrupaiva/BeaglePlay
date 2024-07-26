5cdb09ee92a350aca5a50f53e55c10208b549f58
Daniel Paiva <danielbrug@gmail.com>
2024-07-25 21:14:03 -0300

    WIP PLC Modbus

--- App/globals.hpp
+++ App/globals.hpp
@@ -28,5 +28,15 @@ enum class eSystemState { SCREEN1, SCREEN2, SCREEN3, SCREEN4, SCREEN5 };
 static eSystemState current_state = eSystemState::SCREEN2;
 static std::vector<std::unique_ptr<IScreen>> FSM;
 //PLC Modbus
-static Modbus plc{"127.0.0.1", 1502};
+static std::map<std::string, PLC::Modbus::Data> variable_list = {
+    {"var0", {PLC::Modbus::DataType::COIL, 0, false}},
+    {"var1", {PLC::Modbus::DataType::COIL, 1, false}},
+    {"var2", {PLC::Modbus::DataType::COIL, 2, false}},
+    {"var3", {PLC::Modbus::DataType::COIL, 3, false}},
+    {"var4", {PLC::Modbus::DataType::HOLDING_REGISTER, 0, 0}},
+    {"var5", {PLC::Modbus::DataType::HOLDING_REGISTER, 1, 0}},
+    {"var6", {PLC::Modbus::DataType::HOLDING_REGISTER, 2, 0}},
+    {"var7", {PLC::Modbus::DataType::HOLDING_REGISTER, 3, 0}},
+};
+static PLC::Modbus plc{"TM241", "127.0.0.1", 1502, variable_list};
 };//namespace Global
--- Core/modbus.hpp
+++ Core/modbus.hpp
@@ -1,6 +1,25 @@
 //
 // Created by daniel on 7/24/24.
 //
+// References
+// https://libmodbus.org/
+// https://github.com/stephane/libmodbus
+// https://www.fernhillsoftware.com/help/drivers/modbus/modbus-protocol.html
+// https://github.com/fz-lyu/modbuspp/tree/master
+
+
+/*-------------------------------------------------------------------------------------------------------------------------------*/
+// Data Type            Access      Code        Method                          Description
+// Discrete Input       Read-only   0x02 (02)   modbus_read_input_bits    	    Read Physical Discrete Inputs.
+// Coil	                Read-write  0x01 (01)   modbus_read_coils               Read Internal Bits or Physical Coils
+//                                  0x05 (05)   modbus_write_coil               Write Internal Bits or Physical Coils
+//                                  0x0F (15)   modbus_write_coils              Write Multiple Internal Bits or Physical Coils
+/*-------------------------------------------------------------------------------------------------------------------------------*/
+// Input Register       Read-only   0x04 (04)   modbus_read_input_registers     Read Physical Input Registers 16-bit
+// Holding Register     Read-write  0x03 (03)   modbus_read_holding_registers  	Read Multiple Holding 16-bit registers
+//                                  0x06 (06)   modbus_write_register           Write Single Holding 16-bit registers
+//                                  0x10 (16)   modbus_write_registers          Write Multiple Holding 16-bit registers
+/*-------------------------------------------------------------------------------------------------------------------------------*/
 
 #ifndef MODBUS_HPP
 #define MODBUS_HPP
@@ -9,93 +28,151 @@
 #include "logger.hpp"
 #include "modbus/modbus.h"
 
+namespace PLC
+{
 class Modbus
 {
-    const std::string    TAG                     = "MODBUS";
-    modbus_t *           m_context               = nullptr;
-    std::string          m_server_ip             = "127.0.0.1";
-    int32_t              m_server_port           = 502;
-    uint8_t              m_is_server_connected   = false;
+public:
+    enum class ContextType { TCP };
+    enum class DataType { COIL, DISCRETE_INPUT, INPUT_REGISTER, HOLDING_REGISTER };
+
+    struct Data{
+        DataType data_type;
+        uint16_t address;
+        uint16_t value;
+    };
+private:
+    const std::string               m_TAG                   = "PLC";
+    modbus_t *                      m_context               = nullptr;
+    std::string                     m_server_ip             = "127.0.0.1";
+    int32_t                         m_server_port           = 502;
+    uint8_t                         m_is_connected   = false;
+    std::map<std::string, Data>     m_variables             = {};
 
 public:
     ~Modbus()
     {
         if(m_context)
         {
-            modbus_close(m_context);
+            // disconnect to plc and free resources
+            disconnect(m_context);
             modbus_free(m_context);
         }
-        m_is_server_connected = false;
-        logger.debug(TAG, "Modbus connection closed");
+        logger.debug(m_TAG, "Modbus destroyed");
     }
-    explicit Modbus( const std::string_view ip, const int32_t port )
-        : m_server_ip{ip}, m_server_port{port}
+    explicit Modbus( const std::string& TAG )
+        : m_TAG{TAG}
     {
         logger.debug(TAG, "Modbus constructed");
-
-        m_context = modbus_new_tcp(m_server_ip.c_str(), m_server_port);
-        logger.debug(TAG, "Modbus context created");
-
-        if( modbus_connect(m_context) == -1 )
-        {
-            std::string msg = "Fail to connect to server";
-            logger.error(TAG, msg);
-            throw std::runtime_error(TAG + ": " + msg);
-        }
-        logger.debug(TAG, "Modbus connection established with server: " + m_server_ip + ":" + std::to_string(m_server_port));
-        m_is_server_connected = true;
+        create_context(ContextType::TCP, m_server_ip, m_server_port);
+        connect(m_context, m_server_ip, m_server_port);
     }
 
+    explicit Modbus( const std::string& TAG, const std::string_view ip, const int32_t port)
+        : m_TAG{TAG}, m_server_ip{ip}, m_server_port{port}
+    {
+        logger.debug(TAG, "Modbus constructed");
+        create_context(ContextType::TCP, m_server_ip, m_server_port);
+        connect(m_context, m_server_ip, m_server_port);
+    }
+
+    explicit Modbus( const std::string& TAG, const std::string_view ip, const int32_t port, const std::map<std::string, Data>& variables )
+        : m_TAG{TAG}, m_server_ip{ip}, m_server_port{port}, m_variables{variables}
+    {
+        logger.debug(TAG, "Modbus constructed");
+        create_context(ContextType::TCP, m_server_ip, m_server_port);
+        connect(m_context, m_server_ip, m_server_port);
+    }
+
+public:
+
+    void create_context(ContextType context_type, const std::string& ip, const int32_t port)
+    {
+        if( context_type == ContextType::TCP )
+        {
+            m_context = modbus_new_tcp(ip.c_str(), port);
+            logger.debug(m_TAG, "Modbus context created");
+        }
+    }
+
+    int32_t connect(modbus_t* context, const std::string& ip, const int32_t port)
+    {
+        int rc = -1;
+        if( context ) { rc = modbus_connect( context ); }
+
+        if( rc == -1 )
+        {
+            std::string msg = "Fail to connect to server";
+            logger.error(m_TAG, msg);
+            throw std::runtime_error(m_TAG + ": " + msg);
+        }
+        m_is_connected = true;
+        logger.debug(m_TAG, "Modbus connection established with server: " + ip + ":" + std::to_string(port));
+        return rc;
+    }
+    // Disconnect to plc context ONLY.
+    void disconnect(modbus_t* context)
+    {
+        if(context)
+        {
+            modbus_close(context);
+        }
+        m_is_connected = false;
+        logger.debug(m_TAG, "Modbus connection closed");
+    }
+
+public:
     int32_t read_bits(int addr, int nb, uint8_t * buffer)
     {
         int rc = -1;
-        if(m_is_server_connected)
+        if(m_is_connected)
         {
-            rc = modbus_read_bits(m_context, addr, nb, buffer); // modbus_read_bits(modbus_t *ctx, int addr, int nb, uint8_t *dest);
+            rc = modbus_read_bits(m_context, addr, nb, buffer);
             if(rc == -1)
             {
                 auto msg = "Modbus error during coil(s) read";
-                logger.error(TAG, msg);
-                throw std::runtime_error(TAG + ": " + msg);
+                logger.error(m_TAG, msg);
+                throw std::runtime_error(m_TAG + ": " + msg);
             }
         }
-        logger.debug(TAG, "Success coil(s) read");
+        logger.debug(m_TAG, "Success coil(s) read");
         return rc;
     }
 
     int32_t write_bits(const int addr, const int nb, const uint8_t * data )
     {
         int32_t rc = -1;
-        if(m_is_server_connected)
+        if(m_is_connected)
         {
             rc = modbus_write_bits(m_context, addr, nb, data);
             if(rc == -1)
             {
                 auto msg = "Modbus error during coil(s) write";
-                logger.error(TAG, msg);
-                throw std::runtime_error(TAG + ": " + msg);
+                logger.error(m_TAG, msg);
+                throw std::runtime_error(m_TAG + ": " + msg);
             }
         }
-        logger.debug(TAG, "Success coil(s) write");
+        logger.debug(m_TAG, "Success coil(s) write");
         return rc;
     }
 
     int32_t write_registers(const int addr, const int nb, const uint16_t * data)
     {
         int32_t rc = -1;
-        if(m_is_server_connected)
+        if(m_is_connected)
         {
             rc = modbus_write_registers(m_context, addr, nb, data);
             if(rc == -1)
             {
                 auto msg = "Modbus error during register(s) write";
-                logger.error(TAG, msg);
+                logger.error(m_TAG, msg);
                 throw std::runtime_error(msg);
             }
         }
-        logger.debug(TAG, "Success register(s) write");
+        logger.debug(m_TAG, "Success register(s) write");
         return rc;
     }
 };
+}; // namespace Modbus
 
 #endif //MODBUS_HPP
