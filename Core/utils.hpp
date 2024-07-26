650ed786aca6cca342d38a75fbef6d2b2f38415e
Daniel Paiva <danielbrug@gmail.com>
2024-07-26 12:57:19 -0300

    Add Utils

---
+++ Core/utils.hpp
@@ -0,0 +1,27 @@
+//
+// Created by daniel on 7/26/24.
+//
+
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+// Macro for maximum of two values
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+// Macro for minimum of two values
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+// Macro for average of two values
+#define AVG(x, y) (((x) + (y)) / 2)
+
+#define TOGGLE(var) (var = ! var)
+
+namespace Core
+{
+
+class Utils
+{
+
+};
+
+} // Core
+
+#endif //UTILS_HPP
