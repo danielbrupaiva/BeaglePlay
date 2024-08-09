137a7439d96601643fe9cb037089e51c5109e459
Daniel Paiva <danielbrug@gmail.com>
2024-08-09 17:03:34 -0300

    Fix namespace at Globals

--- App/globals.hpp
+++ App/globals.hpp
@@ -19,24 +19,24 @@ enum class eSystemState { SCREEN1, SCREEN2, SCREEN3, SCREEN4, SCREEN5 };
 static eSystemState current_state = eSystemState::SCREEN2;
 static std::vector<std::unique_ptr<IScreen>> FSM;
 //PLC Modbus
-static std::map<std::string, PLC::Modbus::Data> variable_list = {
-    {"bit0", {PLC::Modbus::Data::Type::COIL, 0U, false}},
-    {"bit1", {PLC::Modbus::Data::Type::COIL, 1U, false}},
-    {"bit2", {PLC::Modbus::Data::Type::COIL, 2U, false}},
-    {"bit3", {PLC::Modbus::Data::Type::COIL, 3U, false}},
-    {"bit4", {PLC::Modbus::Data::Type::COIL, 4U, false}},
-    {"bit5", {PLC::Modbus::Data::Type::COIL, 5U, false}},
-    {"bit6", {PLC::Modbus::Data::Type::COIL, 6U, false}},
-    {"bit7", {PLC::Modbus::Data::Type::COIL, 7U, false}},
-    {"reg0", {PLC::Modbus::Data::Type::HOLDING_REGISTER, 0U, 0}},
-    {"reg1", {PLC::Modbus::Data::Type::HOLDING_REGISTER, 1U, 0}},
-    {"reg2", {PLC::Modbus::Data::Type::HOLDING_REGISTER, 2U, 0}},
-    {"reg3", {PLC::Modbus::Data::Type::HOLDING_REGISTER, 3U, 0}},
-    {"reg4", {PLC::Modbus::Data::Type::HOLDING_REGISTER, 4U, 0}},
-    {"reg5", {PLC::Modbus::Data::Type::HOLDING_REGISTER, 5U, 0}},
-    {"reg6", {PLC::Modbus::Data::Type::HOLDING_REGISTER, 6U, 0}},
-    {"reg7", {PLC::Modbus::Data::Type::HOLDING_REGISTER, 7U, 0}},
+static std::map<std::string, Core::PLC::Modbus::Data> variable_list = {
+    {"bit0", {Core::PLC::Modbus::Data::Type::COIL, 0U, false}},
+    {"bit1", {Core::PLC::Modbus::Data::Type::COIL, 1U, false}},
+    {"bit2", {Core::PLC::Modbus::Data::Type::COIL, 2U, false}},
+    {"bit3", {Core::PLC::Modbus::Data::Type::COIL, 3U, false}},
+    {"bit4", {Core::PLC::Modbus::Data::Type::COIL, 4U, false}},
+    {"bit5", {Core::PLC::Modbus::Data::Type::COIL, 5U, false}},
+    {"bit6", {Core::PLC::Modbus::Data::Type::COIL, 6U, false}},
+    {"bit7", {Core::PLC::Modbus::Data::Type::COIL, 7U, false}},
+    {"reg0", {Core::PLC::Modbus::Data::Type::HOLDING_REGISTER, 0U, 0}},
+    {"reg1", {Core::PLC::Modbus::Data::Type::HOLDING_REGISTER, 1U, 0}},
+    {"reg2", {Core::PLC::Modbus::Data::Type::HOLDING_REGISTER, 2U, 0}},
+    {"reg3", {Core::PLC::Modbus::Data::Type::HOLDING_REGISTER, 3U, 0}},
+    {"reg4", {Core::PLC::Modbus::Data::Type::HOLDING_REGISTER, 4U, 0}},
+    {"reg5", {Core::PLC::Modbus::Data::Type::HOLDING_REGISTER, 5U, 0}},
+    {"reg6", {Core::PLC::Modbus::Data::Type::HOLDING_REGISTER, 6U, 0}},
+    {"reg7", {Core::PLC::Modbus::Data::Type::HOLDING_REGISTER, 7U, 0}},
 };
 
-PLC::Modbus plc{"TM241", "127.0.0.1", 1502, variable_list};
+Core::PLC::Modbus plc{"TM241", "127.0.0.1", 1502, 5U, variable_list};
 };//namespace Global
